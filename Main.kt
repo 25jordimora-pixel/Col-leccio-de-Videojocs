bfc9c4f86f05f1991a4508db98deeef18e65d028
25jordimora-pixel <25.jordi.mora@insdanielblanxart.cat>
2026-03-19 20:01:11 +0100

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,32 @@
+package org.example
+
+import org.example.model.Colleccio
+import org.example.model.Item
+import org.example.model.Plataforma
+import org.example.model.Usuari
+import org.example.model.Videojoc
+
+
+fun main() {
+    val item: Item = Videojoc("1", "Mario Bros", "Aventura", 150, "1985")
+    val colleccio = Colleccio("Favorits")
+    val plataforma = Plataforma(
+        id = item.id, nom = item.nom,
+        fabricant = "Nintendo",
+        anySortida = "1981"
+    )
+    val usuari = Usuari(
+        id = item.id, nom = item.nom, email = String.format("%s %s", plataforma.nom, item.nom),
+    )
+
+    println(item.mostrarInfo())
+    println(colleccio.videojocs.size)
+    println(plataforma.anySortida)
+    println(plataforma.nom)
+    println(plataforma.fabricant)
+    println(usuari.nom)
+    println(usuari.email)
+}
+
+
+
